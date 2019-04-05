bc5e33a0a6785772b89f1375feefea0b876619b8
Sachin Mahajan <sachinm78@gmail.com>
2019-04-05 21:01:24 +0000

    Automatically backed up by Learn

---
+++ lib/game.rb
@@ -0,0 +1,22 @@
+class Game
+    attr_accessor :board
+        
+    def initialize()
+
+        reset_board
+
+    end
+
+    def board
+    
+      game = Game.new
+      game.board = []
+      
+    end
+
+    
+  end
+
+    
+
+end
