75d05a29a48718af23d21d33df9807bb13f24c1b
Sachin Mahajan <sachinm78@gmail.com>
2019-04-05 21:04:04 +0000

    Automatically backed up by Learn

--- lib/board.rb
+++ lib/board.rb
@@ -12,13 +12,19 @@ class Board
     [6,4,2]
   ]
     
+    def cells
+      
+    end
+    
+    
+    
     def display 
         puts " #{@board[0]} | #{@board[1]} | #{@board[2]} "
         puts "-----------"
         puts " #{@board[3]} | #{@board[4]} | #{@board[5]} "
         puts "-----------"
         puts " #{@board[6]} | #{@board[7]} | #{@board[8]} "
-      end
+    end
     
     
     def reset!
